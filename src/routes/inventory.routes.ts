c8f1b6a298b1072f06318a130065afccba50410b
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-07-22 03:11:53 -0300

    add editModel functionality

--- src/routes/inventory.routes.ts
+++ src/routes/inventory.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { addModel } from "../services/inventory.service";
+import { addModel, editModel } from "../services/inventory.service";
 import { verifyUser } from "../services/auth.service";
 
 const router = express.Router()
 
 export default router
 
-router.post("/", verifyUser, addModel)
+router.post("/model", verifyUser, addModel)
+
+router.put("/model", verifyUser, editModel)
--- src/services/inventory.service.ts
+++ src/services/inventory.service.ts
@@ -4,22 +4,43 @@ import { handleError } from "../utils/handleErrors";
 import { inventoryModel, productModel } from "../mongoose";
 
 type modelReq = {
-  model: Inventory,
-  product_id: string  
-}
+  model: Inventory;
+  product_id?: string;
+};
 
 export const addModel = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
   try {
-    const { model, product_id }: modelReq = req.body
-    const addModel = await inventoryModel.create(model)
-    const editProduct = await productModel.findByIdAndUpdate(product_id, {
-      $push: { inventory: { _id: addModel._id } }
-    }, { new: true });
-    if(editProduct) return res.send(editProduct);
-    return res.sendStatus(404)
+    const { model, product_id }: modelReq = req.body;
+    const addModel = await inventoryModel.create(model);
+    const editProduct = await productModel.findByIdAndUpdate(
+      product_id,
+      {
+        $push: { inventory: { _id: addModel._id } },
+      },
+      { new: true }
+    );
+    if (editProduct) return res.send(editProduct);
+    return res.sendStatus(404);
+  } catch (error) {
+    return handleError(error, res);
+  }
+};
+
+export const editModel = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { model }: modelReq = req.body;
+    const { _id, ...query } = model;
+    const editModel = await inventoryModel.findByIdAndUpdate(_id, query, {
+      new: true,
+    });
+    if (editModel) return res.send(editModel);
+    return res.sendStatus(404);
   } catch (error) {
     return handleError(error, res);
   }
