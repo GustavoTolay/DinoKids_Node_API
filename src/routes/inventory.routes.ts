8de16c104355ea989ddb842f55180b6f0c03eaf9
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-07-22 07:23:03 -0300

    fix inventory routes

--- src/routes/inventory.routes.ts
+++ src/routes/inventory.routes.ts
@@ -16,10 +16,10 @@ modelRouter.post("/", verifyUser, addModel);
 
 modelRouter.put("/", verifyUser, editModel);
 
-modelRouter.delete("/", verifyUser, deleteModelById);
+modelRouter.delete("/:id", verifyUser, deleteModelById);
 
 sizeRouter.post("/", verifyUser, addSize);
 
 sizeRouter.put("/", verifyUser, editSize);
 
-sizeRouter.delete("/", verifyUser, deleteSizeById);
+sizeRouter.delete("/:id", verifyUser, deleteSizeById);
