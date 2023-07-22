27f5c739dcea86bad43f272baff41e20dae385f4
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-07-21 21:17:12 -0300

    add delete and put endpoints

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllCategories, addCategory } from "../services/categories.service";
+import { getAllCategories, addCategory, deleteCategoryById, updateCategory } from "../services/categories.service";
 import { verifyUser } from "../services/auth.service";
 
 const router = express.Router();
@@ -9,3 +9,7 @@ export default router;
 router.get("/", getAllCategories);
 
 router.post("/", verifyUser, addCategory);
+
+router.delete("/:id", verifyUser, deleteCategoryById);
+
+router.put("/", verifyUser, updateCategory)
