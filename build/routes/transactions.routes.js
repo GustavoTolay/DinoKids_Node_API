e122758a98c473ae04ca43c23d9db6fcef60e5a3
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-08-08 19:20:27 -0300

    update build 1/2

--- build/routes/transactions.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const products_service_1 = require("../services/products.service");
-const transactions_service_1 = require("../services/transactions.service");
-const router = (0, express_1.Router)();
-router.post("/", products_service_1.reserveStock, transactions_service_1.createTransaction);
-exports.default = router;
